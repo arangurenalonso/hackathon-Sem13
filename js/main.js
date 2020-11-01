9c5a2b4dbc664cded105a2386db8b788e16b90c2
Jose Alonso Aranguren Martinez <aranguren.alonso@gmail.com>
2020-10-31 19:52:54 -0500

    finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const CardContenedor=document.querySelector('.contenedorCard');
 const ModalCerrar=document.querySelector('.modalContenedorCerrar');
+const ModalEdit=document.querySelector('.modalContenedorEdit');
+const ModalCrear=document.querySelector('.modalContenedorCrear');
 
 initApp();
 function initApp(){
@@ -8,12 +10,131 @@ function initApp(){
 
 function accionesCard(e){
     if(e.target.classList.contains('card-icono-imgCerrar')){
-        let card=e.target.parentElement.parentElement
-        ModalCerrar.classList.remove('ocultarModalCerrar')
-        accionesModelCerrar(card)
+        let card=e.target.parentElement.parentElement;
+        ModalCerrar.classList.remove('ocultarModalCerrar');
+        accionesModelCerrar(card);
                    }
-                }   
-            
+    if(e.target.classList.contains('card-icono-imgEdit')){
+        let card=e.target.parentElement.parentElement;
+        
+        ModalEdit.classList.remove('ocultarModalEdit');
+        accionesModalEditar(card);
+                    }
+if(e.target.classList.contains('letraCard--agregar')){
+        let cardContenedor=e.target.parentElement.parentElement;
+        ModalCrear.classList.remove('ocultarModalCrear');
+        accionesModalCrear(cardContenedor);
+                     }
+                       }   
+function accionesModalCrear(cardContenedor){
+    ModalCrear.addEventListener('click', (e)=>{
+
+        //Cerrar el modal de editar
+        if(e.target.classList.contains('modalCrear-btn--Cancelar')||
+            e.target.classList.contains('modalCrear-imgX')||
+            e.target.classList.contains('modalContenedorCrear')){
+            cerrarModal();
+                    }
+        //Aceptar el modal
+        if(e.target.classList.contains('modalCrear-btn--Aceptar')){
+            let modalCrearArg=e.target.parentElement.parentElement;
+
+            btnmodalCrearProcesar(modalCrearArg)
+        }
+})
+
+}
+
+function accionesModalEditar(card){
+    colocarDatosAlForomulario(card)
+    ModalEdit.addEventListener('click', (e)=>{
+
+        //Cerrar el modal de editar
+        if(e.target.classList.contains('modalEdit-btn--Cancelar')||
+            e.target.classList.contains('modalEdit-imgX')||
+            e.target.classList.contains('modalContenedorEdit')){
+            cerrarModal();
+                    }
+        //Aceptar el modal
+        if(e.target.classList.contains('modalEdit-btn--Aceptar')){
+            let modalEditar=e.target.parentElement.parentElement;
+            procesarCambio(modalEditar, card)
+        }
+
+
+    })
+}
+function procesarCambio(modalEditar, card){
+
+    const obtenerInfIngresado={
+        nombre: modalEditar.querySelector(".modalEdit-secForm-formNombre").value,
+        apellido: modalEditar.querySelector(".modalEdit-secForm-formApellido").value,
+        correo: modalEditar.querySelector(".modalEdit-secForm-formCorreo").value,
+        telefono: modalEditar.querySelector(".modalEdit-secForm-formTelef").value,
+        pais: modalEditar.querySelector(".modalEdit-secForm-formPais").value,
+        imgURL:modalEditar.querySelector(".modalEdit-secForm-formUrl"),
+        about: modalEditar.querySelector(".modalEdit-secForm-formTxtArea").value
+    }
+    
+    card.querySelector(".letraCard--Nombre").textContent=obtenerInfIngresado.nombre
+    card.querySelector(".letraCard--Apellido").textContent=obtenerInfIngresado.apellido
+    card.querySelector(".letraCard--Telf").textContent=obtenerInfIngresado.telefono
+    card.querySelector(".letraCard--Corr").textContent=obtenerInfIngresado.correo
+    card.querySelector(".letraCard--Pais").textContent=obtenerInfIngresado.pais
+    card.querySelector(".letraCard--About").textContent=obtenerInfIngresado.about
+    
+    console.log(obtenerInfIngresado.imgURL.files[0])
+    
+    
+    if(obtenerInfIngresado.imgURL.files[0]==undefined){
+
+    }else {
+        let imagenNueva
+        imagenNueva=URL.createObjectURL(obtenerInfIngresado.imgURL.files[0])
+        //card.querySelector(".card-img-principal").setAttribute('src',`${imagenNueva}`)
+        
+        card.querySelector(".card-fotoContenedor").innerHTML=`
+        <img class="card-img " src="${imagenNueva}" alt="">
+       
+        `
+    }
+ 
+    cerrarModal();
+   
+}
+
+function colocarDatosAlForomulario(card){
+    const info = {
+        nombre : card.querySelector('.letraCard--Nombre').textContent,
+        apellido : card.querySelector('.letraCard--Apellido').textContent,
+        telefono : card.querySelector('.letraCard--Telf').textContent,
+        correo : card.querySelector('.letraCard--Corr').textContent,
+        pais : card.querySelector('.letraCard--Pais').textContent,
+        about : card.querySelector('.letraCard--About').textContent,
+        url:card.querySelector('.card-img').src
+     }
+
+    let indiceLista; 
+    PaisObtenido=info.pais
+    switch(PaisObtenido){
+       case PaisObtenido=='Perú': indiceLista=0;break;
+       case PaisObtenido=='Venezuela': indiceLista=1;break;
+       case PaisObtenido=='Argentina': indiceLista=2;break;
+       case PaisObtenido=='Brasil': indiceLista=3;break;
+       case PaisObtenido=='Argentina': indiceLista=4;break;
+       default: indiceLista=5
+    }
+    ModalEdit.querySelector(".modalEdit-secForm-formNombre").value=info.nombre
+    ModalEdit.querySelector(".modalEdit-secForm-formApellido").value=info.apellido
+    ModalEdit.querySelector(".modalEdit-secForm-formCorreo").value=info.correo
+    ModalEdit.querySelector(".modalEdit-secForm-formTelef").value=info.telefono
+    ModalEdit.querySelector(".modalEdit-secForm-formPais").selectedIndex=indiceLista
+    
+    ModalEdit.querySelector(".modalEdit-secForm-formTxtArea").value=info.about
+   
+
+}
+
 
 function accionesModelCerrar(card){
    
@@ -27,81 +148,83 @@ function accionesModelCerrar(card){
                     card.remove();
                     cerrarModal();
                         }
-    })
-
-    
-                        } 
-
-function cerrarModal(){
-    const modal=document.querySelector('.modalContenedorCerrar')
-    modal.classList.add('ocultarModalCerrar')
-        }
-/*
-const activarModal=document.querySelector('#activarModal');
-const contenedorModal=document.querySelector('.modalContenedor');
-
-initApp();
-function initApp(){
-    activarModal.addEventListener('click', abriModal)
-    contenedorModal.addEventListener('click', accionesModal)
-
-
-}
-function abriModal(){
-    const modal=document.querySelector('.modalContenedor')
-    modal.classList.remove('ocultar')
-        }  
-function accionesModal(e){
-    e.preventDefault();
-    if(e.target.classList.contains('modal-btn--Close')||
-    e.target.classList.contains('modal-imgX')||
-    e.target.classList.contains('modalContenedor')){
-        cerrarModal();
-                }    
-    if(e.target.classList.contains('modal-btn--Save')){
-        guardarModal(e)
-        }
-            }   
-
-function cerrarModal(){
-    const modal=document.querySelector('.modalContenedor')
-    modal.classList.add('ocultar')
-}
-function guardarModal(elemento){
-   let valorIngresado=elemento.target.parentElement.parentElement
-   
-   const info = {
-    valor : valorIngresado.querySelector('.modal-input').value
     }
-    if(info.valor==""){
-        valorIngresado.querySelector('.modal-input').classList.add("advertencia")
-    }else{
-    const textoCambiar=document.querySelector('#texto')
-    textoCambiar.innerHTML=info.valor
-    valorIngresado.querySelector('.modal-input').value=""
-    valorIngresado.querySelector('.modal-input').classList.remove("advertencia")
+    )
+} 
 
-    cerrarModal()
-}
+function cerrarModal(){
+    const modal1=document.querySelector('.modalContenedorCerrar')
+    modal1.classList.add('ocultarModalCerrar')
+    const modal2=document.querySelector('.modalContenedorEdit')
+    modal2.classList.add('ocultarModalEdit')
+    const modal3=document.querySelector('.modalContenedorCrear')
+    modal3.classList.add('ocultarModalCrear')
+        }
 
-function agregarLista(e){
-    const listaNoCompletado=document.querySelector('#NoCompletado')
-    
-    const input=document.querySelector('#input')
 
-    if(!(input.value=='')){
 
-        const info={
-            descripcion:input.value,
+function resetearValoresDelModal(modalEditar){
+    modalEditar.querySelector(".modalEdit-secForm-formNombre").value=""
+    modalEditar.querySelector(".modalEdit-secForm-formApellido").value=""
+    modalEditar.querySelector(".modalEdit-secForm-formCorreo").value=""
+    modalEditar.querySelector(".modalEdit-secForm-formTelef").value=""
+    modalEditar.querySelector(".modalEdit-secForm-formPais").value=""
+    //ModalEdit.querySelector(".modalEdit-secForm-formUrl").files[0] =info.nombre
+    modalEditar.querySelector(".modalEdit-secForm-formTxtArea").value=""
+        }
+
+
+function btnmodalCrearProcesar(modalCrear){
+const obtenerInfIngresado={
+    nombre: modalCrear.querySelector(".modalCrear-secForm-formNombre").value,
+    apellido: modalCrear.querySelector(".modalCrear-secForm-formApellido").value,
+    correo: modalCrear.querySelector(".modalCrear-secForm-formCorreo").value,
+    telefono: modalCrear.querySelector(".modalCrear-secForm-formTelef").value,
+    pais: modalCrear.querySelector(".modalCrear-secForm-formPais").value,
+    imgURL:modalCrear.querySelector(".modalCrear-secForm-formUrl"),
+    about: modalCrear.querySelector(".modalCrear-secForm-formTxtArea").value
             }
+    //Crear nuevo card
+    let imagenNueva
 
-        const nuevaLista=document.createElement('li')
-        nuevaLista.innerHTML=`<li class="TareaLista-item TareaLista-item--NoCompletado">${info.descripcion}
-                <button class="TareaLista-Boton agregarACompletado" >agregar</button>
-                <button class="TareaLista-Boton eliminarlista">eliminar</button>
-            </li>
-            `
-        listaNoCompletado.appendChild(nuevaLista)
+    if(obtenerInfIngresado.imgURL.files[0]==undefined){
+        imagenNueva=""
+    }else {
+        imagenNueva=URL.createObjectURL(obtenerInfIngresado.imgURL.files[0])
+        
     }
-}
-}*/
+    const template=document.createElement('div')
+        template.innerHTML=`
+            <div class="card">
+            <div class="icono icono--left">
+                <img class="icono-img card-icono-imgEdit" src="img/edit.png" alt="">
+                <p class="letraCard letraCard--icono">Edit</p>
+            </div>
+            <div class="icono icono--right">
+                <img class="icono-img card-icono-imgCerrar" src="img/delete.svg" alt="">
+                <p class="letraCard letraCard--icono">Delete</p>
+            </div>
+            <div class="card-fotoContenedor">
+                <img class="card-img " src="${imagenNueva}" alt="">
+
+            </div>
+            
+            <div class="card-contenido">
+                <p class="letraCard">
+                    <span class="letraCard--Nombre">${obtenerInfIngresado.nombre}</span>   
+                    <span class="letraCard--Apellido">${obtenerInfIngresado.apellido}</span>
+                    
+                </p>
+                <p class="letraCard">
+                    <span class="letraCard--Telf">${obtenerInfIngresado.telefono}</span>
+                    |
+                    <span class="letraCard--Corr">${obtenerInfIngresado.correo}</span> 
+                </p>
+                <p class="letraCard letraCard--Pais">${obtenerInfIngresado.pais}</p>
+                <P class="letraCard letraCard--About">${obtenerInfIngresado.about}</P>
+            </div>
+        </div>
+            `
+    CardContenedor.appendChild(template)
+    cerrarModal();
+        }
---
+++ reto1.md
@@ -0,0 +1,12 @@
+¿Por qué evitar los callbacks en las llamadas asíncronas?
+
+Por los callbackhell
+
+¿Qué es una promesa?
+
+ Una promesa representa un valor que puede estar disponible ahora, en el futuro, o nunca.
+ esto puede ser si cumple la promesa o no
+
+¿Qué es el callback hell?
+
+Consiste en múltiples Callbacks anidados que provocan que el código se vuelva difícil de leer y ‘debuggear’; ésta es la principal razón por la cual se debe evitar.
